9ed8043bf9b363ae107bd99214ea97d0903eeb9b
woosteelz <woosteelz@naver.com>
2021-11-23 13:29:42 +0900

    UPDATE [MovieDetail.vue]

--- src/views/movies/MovieDetail.vue
+++ src/views/movies/MovieDetail.vue
@@ -2,7 +2,7 @@
   <div>
     <v-img
       :src="`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`"
-      width="100%"
+      max-height="450"
       gradient="to top right, rgba(0, 0, 0, .8), rgba(0, 0, 0, .8)"
       class="rounded-lg"
     >
@@ -40,52 +40,62 @@
           </div>
         </div>
       </v-card>
-      <div class="my-5">
-        <v-tabs fixed-tabs background-color="dark" dark v-model="tab">
-          <v-tab> overview </v-tab>
-          <v-tab> comment </v-tab>
-          <v-tab> review </v-tab>
-          <v-tab> rating </v-tab>
-        </v-tabs>
-        <v-tabs-items v-model="tab">
-          <!-- overview tab -->
-          <v-tab-item>
-            <v-card flat height="400">
-              <v-card-text>{{ movie.overview }} </v-card-text>
-            </v-card>
-          </v-tab-item>
-          <!-- comment tab -->
-          <v-tab-item>
-            <v-card flat>
-              <v-card-text><h1>comment</h1></v-card-text>
-            </v-card>
-          </v-tab-item>
-          <!-- Review tab -->
-          <v-tab-item>
-            <v-card flat>
-              <v-card-text>{{ movie.overview }}</v-card-text>
-            </v-card>
-          </v-tab-item>
-          <!-- Rating Tab -->
-          <v-tab-item>
-            <v-card flat>
-              <v-card-text>{{ movie.overview }}</v-card-text>
-            </v-card>
-          </v-tab-item>
-        </v-tabs-items>
-      </div>
     </v-img>
+    <div class="">
+      <v-tabs fixed-tabs background-color="dark" dark v-model="tab">
+        <v-tab> overview </v-tab>
+        <v-tab> comment </v-tab>
+        <v-tab> review </v-tab>
+        <v-tab> casting </v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab">
+        <!-- overview tab -->
+        <v-tab-item>
+          <v-card flat height="400">
+            <v-card-text>{{ movie.overview }} </v-card-text>
+          </v-card>
+        </v-tab-item>
+        <!-- comment tab -->
+        <v-tab-item>
+          <div flat height="100%" width="100%">
+            <v-card-text><h1>comment</h1></v-card-text>
+          </div>
+        </v-tab-item>
+        <!-- Review tab -->
+        <v-tab-item>
+          <v-card flat height="50%">
+            <v-card-text>{{ movie.overview }}</v-card-text>
+          </v-card>
+        </v-tab-item>
+        <!-- casting Tab -->
+        <v-tab-item>
+          <v-container class="" height="400" fluid>
+            <v-row>
+              <v-col
+                cols="2"
+                lg="2"
+                sm="3"
+                xs12
+                v-for="actor in actors"
+                :key="actor"
+              >
+                <div class="ma-5">
+                  <div class="text-center">
+                    <v-avatar size="128">
+                      <v-img :src="actor.profile_path"> </v-img>
+                    </v-avatar>
+                  </div>
+                  <h3 class="text-center">{{ actor.name }}</h3>
+                </div>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-tab-item>
+      </v-tabs-items>
+    </div>
   </div>
 </template>
 
-<v-col cols="12" md="4">
-        <v-icon color="red" class="ma-3" v-if="like" @click="like = !like"
-          >mdi-heart-outline</v-icon
-        >
-        <v-icon color="red" class="ma-3" v-else @click="like = !like"
-          >mdi-heart</v-icon
-        >
-      </v-col>
 <script>
 import axios from "axios";
 export default {
@@ -94,6 +104,7 @@ export default {
       tab: null,
       like: false,
       movie: null,
+      actors: [],
     };
   },
   props: {},
@@ -116,6 +127,21 @@ export default {
       .then((res) => {
         console.log(res);
         this.movie = res.data;
+
+        for (let i = 0; i < 20; i++) {
+          axios({
+            method: "get",
+            url: `https://api.themoviedb.org/3/person/${this.movie.actors[i].actor_id}?api_key=1e1628fa2029e5e5102664565f10a845&language=ko-kr`,
+          }).then((response) => {
+            console.log(response);
+            const actor_info = {
+              name: response.data.name,
+              profile_path:
+                "https://image.tmdb.org/t/p/w500" + response.data.profile_path,
+            };
+            this.actors.push(actor_info);
+          });
+        }
       })
       .catch((err) => {
         console.log(err);
